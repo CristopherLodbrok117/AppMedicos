014f6a9d6b7d50f6cfa604e05b1ad077b1f68db0
Cristopher <cristopheralexis.chavez@alumnos.udg.mx>
2025-03-05 19:27:40 -0600

    Updated PatientContidionScreen with custom pickers

--- app/PatientConditionScreen.js
+++ app/PatientConditionScreen.js
@@ -8,13 +8,34 @@ export default function PatientConditionScreen() {
   const [showArea, setShowArea] = useState(false);
   const [vehicleType, setVehicleType] = useState('');
 
-  const [vehicleNum, setVehicleNum] = useState('');
+  /* CustomPicker variables */
+  const [stability, setStability] = useState('');
+  const [patientColor, setPatientColor] = useState('');
+  const [airway, setAirway] = useState('');
+  const [descompression, setDescompression] = useState('');
+  const [side, setSide] = useState('');
+  const [cervical, setCervical] = useState('');
+  const [ventilatoryHelp, setVentilatoryHelp] = useState('');
+  const [oxigenTherapy, setOxigenTherapy] = useState('');
+  const [hemorrhageControl, setHemorrhageControl] = useState('');
+  const [solutionType, setSolutionType] = useState('');
+  const [rcp, setRcp] = useState('');
 
-  const [evaluationItem, setEvaluationItem] = useState('');
+  const stabilityItems = ['Si', 'No'];
+  const patientColors = ['No aplica', 'Rojo', 'Amarillo', 'Verde', 'Negra'];
+  const airways = ['No aplica', 'Aspiración', 'Canula orofaríngenea', 'Canula nasofaríngenea'
+    , 'Intubación endotraqueal', 'Mascarilla laringenea', 'Combitubo', 'Cricotirodotomía por punción'];
+  const descompressions = ['No', 'Si'];
+  const sides = ['Derecho', 'Izquierdo'];
+  const cervicals = ['No aplica', 'Manual', 'Collarín', 'Collarín blando'];
+  const ventilatoryHelpItems = ['No aplica', 'BVM', 'Ventilador automático'];
+  const oxigenTherapyItems = ['No aplica', 'Puntas nasales', 'Mascarilla simple', 'Mascarilla de reservorio', 'Mascarilla venturi'];
+  const hemorrhageControlItems = ['No aplica', 'Presión directa', 'Presión indirecta', 'Gravedad', 'Vendaje compresivo', 'Crioterapia', 'Hemostático'];
+  const solutionsTypes = ['No aplica', 'Hartman', 'NACL 0.9%', 'Mixta', 'Clucosa 5%'];
+  const rcps = ['No aplica', 'RCP básica', 'RCP avanzada'];
 
   
-  const firstEvaluationItems = ['No aplica', 'Consciente', 'Respuesta a estimulo verbal', 'Respuesta a estimulo doloroso', 'Inconsciente'];
-
+  
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -28,10 +49,83 @@ export default function PatientConditionScreen() {
         source={require('./assets/doctor.png')}
       />
       
+      <CustomPicker
+        label='Se encuentra estable'
+        selectedValue={stability}
+        onValueChange={setStability}
+        options={stabilityItems}
+      />
 
-      <Text style={styles.subtitle}>Pendiente a implementación</Text>
+      <CustomPicker
+        label='Color del paciente'
+        selectedValue={patientColor}
+        onValueChange={setPatientColor}
+        options={patientColors}
+      />
 
-      
+      <CustomPicker
+        label='Via aérea'
+        selectedValue={airway}
+        onValueChange={setAirway}
+        options={airways}
+      />
+
+      <CustomPicker
+        label='Descompresión pleural'
+        selectedValue={descompression}
+        onValueChange={setDescompression}
+        options={descompressions}
+      />
+
+      <CustomPicker
+        label='Lado'
+        selectedValue={side}
+        onValueChange={setSide}
+        options={sides}
+      />
+
+      <CustomPicker
+        label='Control cervical'
+        selectedValue={cervical}
+        onValueChange={setCervical}
+        options={cervicals}
+      />
+
+      <CustomPicker
+        label='Asistencia ventilatoria'
+        selectedValue={ventilatoryHelp}
+        onValueChange={setVentilatoryHelp}
+        options={ventilatoryHelpItems}
+      />
+
+      <CustomPicker
+        label='Oxigenoterapía'
+        selectedValue={oxigenTherapy}
+        onValueChange={setOxigenTherapy}
+        options={oxigenTherapyItems}
+      />
+
+      <CustomPicker
+        label='Control de hemorragías'
+        selectedValue={hemorrhageControl}
+        onValueChange={setHemorrhageControl}
+        options={hemorrhageControlItems}
+      />
+
+      <CustomPicker
+        label='Tipo de soluciones'
+        selectedValue={solutionType}
+        onValueChange={setSolutionType}
+        options={solutionsTypes}
+      />
+
+      <CustomPicker
+        label='RCP'
+        selectedValue={rcp}
+        onValueChange={setRcp}
+        options={rcps}
+      />
+
 
       <TouchableOpacity style={styles.saveButton}>
         <Text style={styles.buttonText}>Guardar</Text>
